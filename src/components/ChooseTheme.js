7838a942eddcba1afb5f9429c76faaa52c60b5de
Casper Bornebusch <cahbb9@gmail.com>
2017-08-06 17:53:34 +0200

    added google analytics

--- src/components/ChooseTheme.js
+++ src/components/ChooseTheme.js
@@ -1,14 +1,60 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import '../assets/css/choose-theme.css'
 
 const ChooseTheme = props => {
   return (
     <div className="choose-theme">
-      <div className="theme circle green-purple-horizontal" onClick={ () => props.setTheme('green-purple-horizontal') }></div>
-      <div className="theme circle blue-red-pink-horizontal" onClick={ () => props.setTheme('blue-red-pink-horizontal') }></div>
-      <div className="theme circle yellow-orange-pink-horizontal" onClick={ () => props.setTheme('yellow-orange-pink-horizontal') }></div>
-      <div className="theme circle turquoise-blue-diagonal" onClick={ () => props.setTheme('turquoise-blue-diagonal') }></div>
-      <div className="theme circle blue-red-diagonal" onClick={ () => props.setTheme('blue-red-diagonal') }></div>
+      <div className="theme circle green-purple-horizontal" onClick={
+        () => {
+          props.setTheme('green-purple-horizontal')
+          ReactGA.event({
+            category: 'Themes',
+            action: 'Selected a theme',
+            label: 'green-purple-horizontal'
+          })
+        }
+      }></div>
+      <div className="theme circle blue-red-pink-horizontal" onClick={
+        () => {
+          props.setTheme('blue-red-pink-horizontal')
+          ReactGA.event({
+            category: 'Themes',
+            action: 'Selected a theme',
+            label: 'blue-red-pink-horizontal'
+          })
+        }
+      }></div>
+      <div className="theme circle yellow-orange-pink-horizontal" onClick={
+        () => {
+          props.setTheme('yellow-orange-pink-horizontal')
+          ReactGA.event({
+            category: 'Themes',
+            action: 'Selected a theme',
+            label: 'yellow-orange-pink-horizontal'
+          })
+        }
+      }></div>
+      <div className="theme circle turquoise-blue-diagonal" onClick={
+        () => {
+          props.setTheme('turquoise-blue-diagonal')
+          ReactGA.event({
+            category: 'Themes',
+            action: 'Selected a theme',
+            label: 'turquoise-blue-diagonal'
+          })
+        }
+      }></div>
+      <div className="theme circle blue-red-diagonal" onClick={
+        () => {
+          props.setTheme('blue-red-diagonal')
+          ReactGA.event({
+            category: 'Themes',
+            action: 'Selected a theme',
+            label: 'blue-red-diagonal'
+          })
+        }
+      }></div>
     </div>
   )
 }
